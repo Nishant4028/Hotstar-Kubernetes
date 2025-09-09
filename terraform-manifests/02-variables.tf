82220f421f3adec157d7649b2e8527445ef63949
Nishant Gawande <138497034+Nishant4028@users.noreply.github.com>
2025-09-09 18:13:32 +0530

    Update 02-variables.tf

--- terraform-manifests/02-variables.tf
+++ terraform-manifests/02-variables.tf
@@ -3,7 +3,7 @@
 variable "location" {
   type = string
   description = "Azure Region where all these resources will be provisioned"
-  default = "Eastus2"
+  default = "Eastus"
 }
 
 # Azure Resource Group Name
